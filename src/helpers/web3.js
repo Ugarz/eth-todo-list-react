9155ab2fb5f739919f9ccd33bf977209d1c639c9
Ugo Arzur <ugo.arzur@gmail.com>
2019-03-07 13:57:42 +0100

    web3 helper

---
+++ src/helpers/web3.js
@@ -0,0 +1,38 @@
+import Accounts from 'web3-eth-accounts'
+import isEmpty from 'lodash/isEmpty';
+import Web3 from 'web3';
+
+const initialize = (client) => new Promise (async (resolve, reject) => {
+    if(!isEmpty(client)) return resovle(client);
+    try {
+        const client = new Web3(Web3.givenProvider || "ws://localhost:8546");
+        const network = await client.eth.net.getNetworkType()
+        const accounts = await client.eth.getAccounts()
+        return resolve({ client, network, accounts })
+    } catch (error) {
+        console.log('Whoops, error while initilizing the web3 client')
+        return reject(error)
+    }
+})
+// const initialize = () => {
+//     const client = new Web3(Web3.givenProvider || "ws://localhost:8546");
+//     const network = await client.eth.net.getNetworkType()
+//     const accounts = await client.eth.getAccounts()
+//     return { client, network, accounts }
+// }
+
+/**
+ * Load some data from the main net
+ */
+// const loadBlockchainData = async (client) => {
+//     const client = new Web3(Web3.givenProvider || "ws://localhost:8546");
+//     const network = await client.eth.net.getNetworkType()
+//     console.log("loadBlockchainData network: %s", network)
+//     const accounts = await client.eth.getAccounts()
+//     console.log('accounts', accounts)
+//     return {network, accounts};
+// }
+
+module.exports = {
+    initialize
+}
